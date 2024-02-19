cd1f5d8d8af9765c7d36b78196969b33014ceb63
ritik <ritikbhatt020@gmail.com>
2024-02-20 02:09:01 +0530

    ⚔️Delete records done..

--- website/views.py
+++ website/views.py
@@ -61,3 +61,13 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be Logged In to view that page...")
         return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be Logged In to do that...")
+        return redirect('home')
